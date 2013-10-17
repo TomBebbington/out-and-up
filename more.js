754829d4d4915c211ecae3b3a206273d4841870d
Tom Bebbington <tophattedcoder@gmail.com>
2013-10-17 08:57:51 -0400

    Some font fixes

--- more.js
+++ more.js
@@ -1,9 +1,14 @@
+var seed = localStorage.treeSeed || (localStorage.treeSeed = Math.floor(Math.random() * 1000));
+function random() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+}
 function makeTree() {
 	var t = new Image();
-	t.src = "http://openclipart.org/image/60px/svg_to_png/1936/harmonic_Tree.png";
+	t.src = "//openclipart.org/image/60px/svg_to_png/1936/harmonic_Tree.png";
 	t.className = "tree";
-	t.style.right = (Math.random() * (window.innerWidth - 50)) + "px";
-	var degs = (6 * Math.random() - 3)+"deg";
+	t.style.right = (random() * (window.innerWidth - 50)) + "px";
+	var degs = (6 * random() - 3)+"deg";
 	t.style.transform = "rotate("+degs+")";
 	document.body.insertBefore(t, document.getElementById("main"));
 }
